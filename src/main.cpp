c8c8749d2acd2026280369c03cf054395515eed4
Akihiro-K <kurosaki-akihiro@g.ecc.u-tokyo.ac.jp>
2017-10-08 12:52:11 +0900

    refactored program

--- src/main.cpp
+++ src/main.cpp
@@ -12,125 +12,171 @@
 #include <string>
 #include <sstream>
 #include <cmath>
+#include <vector>
 
-using namespace cv;
-using namespace aruco;
-using namespace std;
+#include "json.hpp"
+
+using json = nlohmann::json;
+
+// =============================================================================
+// Sample Map Configuration file (.json):
+
+// {
+//   "MKR_0": {
+//     "id": 111,
+//     "centx": 0,
+//     "centy": 0,
+//     "alt": 0,
+//     "size": 0.5
+//   },
+//   "MKR_1": {
+//     "id": 222,
+//     "centx": 1,
+//     "centy": 0,
+//     "alt": 0,
+//     "size": 0.5
+//   }
+// }
+
+// =============================================================================
 
 // =============================================================================
 // Parameters:
-#define mSize 16
-#define mInfo 1
+const int mSize = 0;
+const int mInfo = 0;
 
-static int ID_LIST[] = {111,222,333,444,555,667,777,888,999,11,22,33,44,55,66,77};
-static double CENTX_LIST[] = {-0.995,0,0.995,0,0.26,-0.26,0.18,-0.18,0.28,-0.28,0.1,-0.1,0.87,-0.87,-0.87,0.87};
-static double CENTY_LIST[] = {0,0.995,0,-0.995,0.26,-0.26,-0.39,0.39,-0.13,0.13,-0.1,0.1,0.87,0.87,-0.87,-0.87};
-static double ALT_LIST[] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
-static double SIZE_LIST[] = {0.85,0.85,0.85,0.85,0.42,0.42,0.26,0.26,0.16,0.16,0.1,0.1,0.6,0.6,0.6,0.6};
+std::vector<int> ID_LIST;
+std::vector<double> CENTX_LIST;
+std::vector<double> CENTY_LIST;
+std::vector<double> ALT_LIST;
+std::vector<double> SIZE_LIST;
+
 // =============================================================================
 
-void savetofile(string filepath);
-void dispoutput(string filepath);
-Point3d getcorner(int markernum, int cornernum);
+void readfromfile(std::string filepath);
+void savetofile(std::string filepath);
+void dispoutput(std::string filepath);
+cv::Point3d getcorner(int markernum, int cornernum);
 
 int main(int ac, char** av)
 {
-    string filepath = "./output.yml";
+  std::string filepath_in = "../input_data/configuration.json"
+  std::string filepath_out = "../output_data/map.yml";
 
-    savetofile(filepath);
+  readfromfile(filepath_in);
 
-    dispoutput(filepath);
+  savetofile(filepath_out);
 
-    return 0;
+  dispoutput(filepath_out);
+
+  return 0;
 }
 
-void savetofile(string filepath){
-    FileStorage fs(filepath, cv::FileStorage::WRITE);
-
-    fs << "aruco_bc_nmarkers" << mSize;
-    fs << "aruco_bc_mInfoType" << mInfo;
-    fs << "aruco_bc_markers"
-       << "[";
-
-    for (int i=0; i < mSize; i++) {
-        fs << "{:"
-           << "id" << ID_LIST[i];
-        
-        fs << "corners"
-           << "[:";
-        for (int j = 0; j < 4; j++){
-            Point3d temp=getcorner(i,j); 
-            //fs << setprecision(4) << temp;
-            fs << temp;
-        }
-        fs << "]";
-        fs << "}";
-    }
-    fs << "]";    
+void readfromfile(std::string filepath)
+{
+  std::ifstream ifs(filepath);
+  json j_;
+  ifs >> j_;
+  mSize = j_.size();
+  mInfo = 1;
+  for (int i = 0; i < mSize; i++) {
+    std::ostringstream oss;
+    oss << "MKR_" << i;
+    std::string mkr_name = oss.str();
+    ID_LIST.push_back(j_[mkr_name.c_str()]["id"]);
+    CENTX_LIST.push_back(j_[mkr_name.c_str()]["centx"]);
+    CENTY_LIST.push_back(j_[mkr_name.c_str()]["centy"]);
+    ALT_LIST.push_back(j_[mkr_name.c_str()]["alt"]);
+    SIZE_LIST.push_back(j_[mkr_name.c_str()]["size"]);
+  }
 }
 
-void dispoutput(string filepath){
-    MarkerMap MM;
-    MM.readFromFile(filepath);
-    cout << "generated map is valid" << endl;
-    cout << endl;
+void savetofile(std::string filepath){
+  cv::FileStorage fs(filepath, cv::FileStorage::WRITE);
 
-    Dictionary dic = Dictionary::loadPredefined("ARUCO");
-    cout << "*****************Generated Map********************" << endl;
-    cout << "aruco_bc_nmarkers: " << (int)MM.size() << endl;
-    cout << "aruco_bc_mInfoType: " << (int)MM.mInfoType << endl;;
-    cout << "aruco_bc_markers: " << endl;
-    for (size_t i = 0; i < MM.size(); i++) {
-        cout << i+1;
-        if (i%10==0) {
-            cout << "st marker: "  << "id #" << MM[i].id << endl;
-        } else if (i%10==1) {
-            cout << "nd marker: "  << "id #" << MM[i].id << endl;
-        } else if (i%10==2) {
-            cout << "rd marker: "  << "id #" << MM[i].id << endl;
-        } else {
-            cout << "th marker: "  << "id #" << MM[i].id << endl;
-        }
-        for (size_t c = 0; c < MM[i].size(); c++){
-            cout << "\t" << "corner" << c+1 << ": ";
-            cout << MM[i][c].x << "\t" << MM[i][c].y << "\t" << MM[i][c].z << endl;
-         }
-         ostringstream oss1;
-         oss1 << "#" << MM[i].id;
-         string windowname = oss1.str();
-         cv::imshow(windowname, dic.getMarkerImage_id(MM[i].id,100*SIZE_LIST[i]/SIZE_LIST[0]));
-         cv::waitKey(0);
-         ostringstream oss2;
-         oss2 << MM[i].id << ".png";
-         string filename = oss2.str();
-         cv::imwrite(filename, dic.getMarkerImage_id(MM[i].id,100*SIZE_LIST[i]/SIZE_LIST[0]));    
+  fs << "aruco_bc_nmarkers" << mSize;
+  fs << "aruco_bc_mInfoType" << mInfo;
+  fs << "aruco_bc_markers"
+      << "[";
+
+  for (int i=0; i < mSize; i++) {
+    fs << "{:"
+        << "id" << ID_LIST[i];
+    
+    fs << "corners"
+        << "[:";
+    for (int j = 0; j < 4; j++){
+      cv::Point3d temp=getcorner(i,j); 
+      fs << temp;
     }
-    cout << "************************************************" << endl;    
+    fs << "]";
+    fs << "}";
+  }
+  fs << "]";
 }
 
-Point3d getcorner(int markernum, int cornernum){
-    Point3d temp;
-    switch(cornernum){
-        case 0:
-            temp.x=(CENTX_LIST[markernum]-SIZE_LIST[markernum]/2.0);
-            temp.y=(CENTY_LIST[markernum]+SIZE_LIST[markernum]/2.0);
-            temp.z=ALT_LIST[markernum];
-            break;
-        case 1:
-            temp.x=(CENTX_LIST[markernum]-SIZE_LIST[markernum]/2.0);
-            temp.y=(CENTY_LIST[markernum]-SIZE_LIST[markernum]/2.0);
-            temp.z=ALT_LIST[markernum];
-            break;
-        case 2:
-            temp.x=(CENTX_LIST[markernum]+SIZE_LIST[markernum]/2.0);
-            temp.y=(CENTY_LIST[markernum]-SIZE_LIST[markernum]/2.0);
-            temp.z=ALT_LIST[markernum];
-            break;
-        case 3:
-            temp.x=(CENTX_LIST[markernum]+SIZE_LIST[markernum]/2.0);
-            temp.y=(CENTY_LIST[markernum]+SIZE_LIST[markernum]/2.0);
-            temp.z=ALT_LIST[markernum];
-            break;;    
+void dispoutput(srd::string filepath){
+  aruco::MarkerMap MM;
+  MM.readFromFile(filepath);
+  std::cout << "generated map is valid" << std::endl;
+  std::cout << std::endl;
+
+  aruco::Dictionary dic = aruco::Dictionary::loadPredefined("ARUCO");
+  std::cout << "*****************Generated Map********************" << std::endl;
+  std::cout << "aruco_bc_nmarkers: " << (int)MM.size() << std::endl;
+  std::cout << "aruco_bc_mInfoType: " << (int)MM.mInfoType << std::endl;;
+  std::cout << "aruco_bc_markers: " << std::endl;
+  for (size_t i = 0; i < MM.size(); i++) {
+    std::cout << i+1;
+    if (i%10==0) {
+        std::cout << "st marker: "  << "id #" << MM[i].id << std::endl;
+    } else if (i%10==1) {
+        std::cout << "nd marker: "  << "id #" << MM[i].id << std::endl;
+    } else if (i%10==2) {
+        std::cout << "rd marker: "  << "id #" << MM[i].id << std::endl;
+    } else {
+        std::cout << "th marker: "  << "id #" << MM[i].id << std::endl;
     }
-    return temp;
+    for (size_t c = 0; c < MM[i].size(); c++){
+        std::cout << "\t" << "corner" << c+1 << ": ";
+        std::cout << MM[i][c].x << "\t" << MM[i][c].y << "\t" << MM[i][c].z << std::endl;
+    }
+    std::ostringstream oss1;
+    oss1 << "#" << MM[i].id;
+    std::string windowname = oss1.str();
+    cv::imshow(windowname, dic.getMarkerImage_id(MM[i].id,100*SIZE_LIST[i]/SIZE_LIST[0]));
+    cv::waitKey(0);
+    std::ostringstream oss2;
+    oss2 << MM[i].id << ".png";
+    std::string filename = oss2.str();
+    cv::imwrite(filename, dic.getMarkerImage_id(MM[i].id,100*SIZE_LIST[i]/SIZE_LIST[0]));
+  }
+  std::cout << "************************************************" << std::endl;
+}
+
+cv::Point3d getcorner(int markernum, int cornernum){
+  cv::Point3d temp;
+  switch(cornernum){
+    case 0:
+      temp.x=(CENTX_LIST[markernum]-SIZE_LIST[markernum]/2.0);
+      temp.y=(CENTY_LIST[markernum]+SIZE_LIST[markernum]/2.0);
+      temp.z=ALT_LIST[markernum];
+      break;
+    case 1:
+      temp.x=(CENTX_LIST[markernum]-SIZE_LIST[markernum]/2.0);
+      temp.y=(CENTY_LIST[markernum]-SIZE_LIST[markernum]/2.0);
+      temp.z=ALT_LIST[markernum];
+      break;
+    case 2:
+      temp.x=(CENTX_LIST[markernum]+SIZE_LIST[markernum]/2.0);
+      temp.y=(CENTY_LIST[markernum]-SIZE_LIST[markernum]/2.0);
+      temp.z=ALT_LIST[markernum];
+      break;
+    case 3:
+      temp.x=(CENTX_LIST[markernum]+SIZE_LIST[markernum]/2.0);
+      temp.y=(CENTY_LIST[markernum]+SIZE_LIST[markernum]/2.0);
+      temp.z=ALT_LIST[markernum];
+      break;
+  }
+  return temp;
 }
